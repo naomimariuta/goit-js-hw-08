8a54b812cd274915f0fc43dba82b3718881df3e1
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2024-07-13 02:03:17 +0300

    last commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.getElementById('vimeo-player');
+const player = new Player(vimeoPlayer);
+console.log(player);
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(currentTime => {
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+  }, 1000)
+);
+
+const time = localStorage.getItem('videoplayer-current-time');
+time === null ? player.setCurrentTime(0) : player.setCurrentTime(time);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({ email, message })
+    );
+  }),
+  500
+);
+
+window.addEventListener('load', () => {
+  const state = localStorage.getItem('feedback-form-state');
+  if (state) {
+    const { email, message } = JSON.parse(state);
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+});
